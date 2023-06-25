9efc49da28272d79f49486e51462033e5fe9ba8b
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-06-25 15:37:21 -0300

    Criando componentes Navbar, Footer e Layout

---
+++ Components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p className="text-center text-dark">
+        © {new Date().getFullYear} Pedro Soares Dias
+      </p>
+    </footer>
+  );
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,23 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import '@/styles/globals.css'
-import { useEffect } from "react"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/globals.css";
+import { useEffect } from "react";
+import Head from "next/head";
+import Layout from "../../Components/Layout";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js")
-  }, [])
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
