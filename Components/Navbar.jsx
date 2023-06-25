7fed91d4742acef98e22c685272f56eb2f9f853f
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-06-25 16:39:00 -0300

    Usando Componente Link para mudar de página

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,29 +1,32 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-link" aria-current="page" href="#">
-                Home
-              </a>
-            </div>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link className="nav-link" aria-current="page" href={"/"}>
+              Inicio
+            </Link>
+            <Link className="nav-link" href={"/JurosCompostos"}>Juros Compostos</Link>
           </div>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 }
