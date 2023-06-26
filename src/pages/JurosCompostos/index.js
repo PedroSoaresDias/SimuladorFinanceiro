9f7030ddd4f7f7bf85afa551200b1798343b5ca7
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-06-26 10:17:41 -0300

    Melhorando o código de juros compostos, os juros anuais são convertido diretamente para juros mensais

--- src/pages/JurosCompostos/index.js
+++ src/pages/JurosCompostos/index.js
@@ -1,17 +1,16 @@
 import { useState } from "react";
 
 export default function JurosCompostos() {
-    const [capital, setCapital] = useState();
-    const [taxa, setTaxa] = useState();
-    const [aportes, setAportes] = useState();
-    const [periodo, setPeriodo] = useState();
-    const [resultado, setResultado] = useState();
+    const [capital, setCapital] = useState("");
+    const [taxaJurosAnual, setTaxaJurosAnual] = useState("");
+    const [valorAporteMensal, setValorAporteMensal] = useState("");
+    const [periodo, setPeriodo] = useState("");
+    const [resultado, setResultado] = useState("");
 
     function CalcularJurosCompostos() {
-        const taxaJuros = taxa / 100;
-        const jurosMensais = taxaJuros / 12;
+        const taxaJurosMensal = taxaJurosAnual / 100 / 12;
 
-        const juros = capital * (Math.pow(1 + jurosMensais, periodo) - 1) + (aportes * (Math.pow(1 + jurosMensais, periodo) - 1) / jurosMensais);
+        const juros = capital * ((1 + taxaJurosMensal) ** periodo - 1) + (valorAporteMensal * ((1 + taxaJurosMensal) ** periodo - 1) / taxaJurosMensal);
 
         const montante = capital + juros;
         setResultado(montante);
@@ -22,15 +21,15 @@ export default function JurosCompostos() {
             <h1>Calculadora de Juros Compostos</h1>
             <input type="number" placeholder="Capital" value={capital} onChange={(e) => setCapital(parseFloat(e.target.value))} />
             <br />
-            <input type="number" placeholder="aportes mensais" value={aportes} onChange={(e) => setAportes(parseFloat(e.target.value))} />
+            <input type="number" placeholder="aportes mensais" value={valorAporteMensal} onChange={(e) => setValorAporteMensal(parseFloat(e.target.value))} />
             <br />
-            <input type="number" placeholder="Taxa de juros (%)" value={taxa} onChange={(e) => setTaxa(parseFloat(e.target.value))} />
+            <input type="number" placeholder="Taxa de juros (% ao ano)" value={taxaJurosAnual} onChange={(e) => setTaxaJurosAnual(parseFloat(e.target.value))} />
             <br/>
             <input type="number" placeholder="Período (em meses)" value={periodo} onChange={(e) => setPeriodo(parseFloat(e.target.value))} />
             <br />
             <button onClick={CalcularJurosCompostos}>Calcular</button>
             {resultado > 0 && (<p>Capital inicial: R$ {capital.toFixed(2)}</p>)}
-            {resultado > 0 && (<p>Total investido: R$ {capital + (aportes * periodo)}</p>)}
+            {resultado > 0 && (<p>Total investido: R$ {(capital + valorAporteMensal * periodo).toFixed(2)}</p>)}
             {resultado > 0 && (<p>O montante é: R$ {resultado.toFixed(2)}</p>)}
         </>
     )
